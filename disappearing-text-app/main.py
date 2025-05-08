3be17931f5cbb45028eeead7b20f94dfa55e5f6c
NiklasDu <n.duden@icloud.com>
2025-05-08 17:40:07 +0200

    Splash Screen hinzugefügt

--- disappearing-text-app/main.py
+++ disappearing-text-app/main.py
@@ -1,13 +1,30 @@
 # ------------------------ Imports ------------------------- #
 from gui.main_window import MainWindow
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QSplashScreen
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QTimer, Qt
+
+# ------------------------ Splash Class ------------------------- #
+
+class SplashScreen(QSplashScreen):
+    def __init__(self, pixmap):
+        super().__init__(pixmap)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
 
 # ------------------------ Program Loop ------------------------- #
 
 app = QApplication([])
 
+pixmap = QPixmap("disappearing-text-app/assets/logo.png").scaled(400, 400, Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation)
+splash = SplashScreen(pixmap)
+splash.show()
+
+QTimer.singleShot(2000, splash.close)
+
+
 window = MainWindow()
-window.show()
+
+QTimer.singleShot(2000, window.show)
 
 app.exec()
 
